0020257f4a4d2410afb59bbdd8424ba36fea1347
7777Satish <onlinegamespartner2@gmail.com>
2025-03-03 18:07:15 +0530

    Fixed bugs

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,15 +3,24 @@ dotenv.config();
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-   host: process.env.DB_HOST || 'localhost',
-   user: process.env.DB_USER || 'root',
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_DATABASE
+    uri: process.env.DATABASE_URL,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
-db.getConnection((err)=>{
-    if(err) console.error(err);
-    else console.log('Connected to database');
-})
+
+(async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log('Connected to database ✅');
+        connection.release();
+    } catch (err) {
+        console.error('Database connection failed ❌', err);
+    }
+})();
 
 export default db;
--- models/userModel.js
+++ models/userModel.js
@@ -2,15 +2,15 @@ import db from '../config/dbConfig.js';
 
 const User = {
     create: (userData)=>{
-        const query = `INSERT INTO USERS (username, fullname, email, password, age ) VALUES(?, ?, ?, ?, ?)`;
+        const query = `INSERT INTO users (username, fullname, email, password, age ) VALUES(?, ?, ?, ?, ?)`;
         return db.query(query, [userData.username, userData.fullname, userData.email, userData.password, userData.age]);
     },
     getByUsername: (username)=>{
-        const query = `SELECT * FROM USERS WHERE USERNAME=?`;
+        const query = `SELECT * FROM users WHERE USERNAME=?`;
         return db.query(query, [username]);
     },
     getByEmail: (email)=>{
-        const query = `SELECT * FROM USERS WHERE EMAIL=?`;
+        const query = `SELECT * FROM users WHERE EMAIL=?`;
         return db.query(query, [email]);
     },
     gettopprofiles: async (username) => {
